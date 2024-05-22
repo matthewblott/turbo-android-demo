b71e4abe5d6cff8df95c0df3a39d534dc944ef0b
Matthew Blott <m4ttb@pm.me>
2024-05-22 13:44:48 +0100

    saved

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,18 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
+  repositories {
+    google()
+    mavenCentral()
+  }
 
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.4.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-    }
+  dependencies {
+    classpath("com.android.tools.build:gradle:8.4.0")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+  }
 }
 
 allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks.register<Delete>("clean").configure {
-    delete(rootProject.buildDir)
+  repositories {
+    google()
+    mavenCentral()
+  }
 }
